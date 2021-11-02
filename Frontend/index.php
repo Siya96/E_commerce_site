e42b2aebde5e51a16bb61118db8f5090a03daff9
Siyabend Revend <Siyarevend@hotmail.com>
2021-11-02 10:03:22 +0100

    New

---
+++ Frontend/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="icon" href="favicon.gif" type="image/gif" sizes="15x15">
+  
+</head>
+<body><div class="border-center"></div>
+
+<div class="bannerContainer">
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="index2.html" id="logo">Bättre bröd AB</a>
+      <div class="dropdown"><a class="dropbtn cart" href="#"></a>
+          <div class="dropdown-content cartDown">
+          <div class="tom"><p>Din shoppingbag är tom!</p></div>
+          <div class="result"><p><b>Beställningsvärde:<b id="p1">0.0kr</b></b></p><hr>
+		  <p><b>Totalsumma:<b id="p3">0.0kr</b></b></p>
+		  </div></div>
+      </div>
+    </div>
+	<div class="collapse navbar-collapse" id="myNavbar">
+	
+	<ul class="nav navbar-nav">
+      <li><a href="index2.html" id="active">Start</a></li>
+      <li class="dropdown"><a class="dropbtn" href="#" id="menu">Sortiment</a>
+          <div class="dropdown-content link">
+          <a href="bakelse.html">Bakelse</a>
+          <a href="bullar.html">Bullar</a>
+		  <a href="bröd.html">Bröd</a>
+      </div></li>
+	  <li><a href="omOss.html" id="menu">Om Oss</a></li>
+    </ul>
+	
+    <ul class="nav navbar-nav navbar-right">
+      <li class="dropdown"><a class="dropbtn cart2" href="shoppingBag.html" id="menu"></a>
+          <div class="dropdown-content cartDown">
+          <div class="tom"><p>Din shoppingbag är tom!</p></div>
+          <div class="result"><p>Beställningsvärde:<b id="p1">0.0kr</b></p><hr>
+		  <p><b>Totalsumma:<b id="p3">0.0kr</b></b></p>
+		  </div>
+      </div></li>
+    </ul>
+  </div></div>
+</nav></div>
+
+
+
+<div class="jumbotron">
+<div class="container-fluid">
+<i>Bättre bröd AB</i><br>
+</div>
+</div><img src="Bröd1.jpeg" alt="Deg som knådas" id="förstB">
+
+<div class="hLine1"></div>
+<div class="vLine1"></div>
+<div class="AB">
+<i> Ett bageri som bryr sig om kvalitet!</i>
+<p>Vi i Bättre Bröd AB bryr oss om kvaliten. Bara varor av högsta kvalieten används för att skapa härliga produkter som 
+ni, kunderna, kan njuta av. Ni kan beställa varor genom att ringa oss eller via hemsidan.</p>
+</div>
+<div class="vLine2"></div>
+<div class="hLine2"></div>
+
+<div class="container bleh">
+<div class="row text-center">
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="wheat.jpeg" alt="Vetestrån">
+          <p><strong>Minst fullkorn!</strong></p>
+         <p>Vi använder misnt fullkorn i varorna eftersom det är bättre för hälsan och mer näringsrikt än vanligt mjöl som gör att personer orkar mer.</p>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="freshIng.jpeg" alt="Färska ingredienser">
+          <p><strong>Färska ingredienser!</strong></p>
+          <p>Färska ingredienser är ett måste i bageriet. De köps dagligen från bönderna i Sverige med deras fräschhet intakt.</p>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="dough.jpg" alt="Deg som knådas">
+          <p><strong>Nybakat varje dag!</strong></p>
+          <p>Varorna är nybakade varje dag för kundernas optimala njutelse av varorna. Resterna ges till dem anställda för att ta hem.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="hLine1"></div>
+<div class="vLine1"></div>
+  <div class="AB">
+<i> Bli medlem och njut av fördelar!</i>
+<p><a href="medlem3.html">Bli medlem</a> och få en hel del av fördelar. Rabatter som kan användas vid köp och ju mer ni köper deto mer rabatter med höga värden blir det. Fri frakt
+vid val av att leverera hem och nythetbrev av inkommande nya varor. </p>
+</div><div class="vLine2"></div>
+<div class="hLine2"></div>
+
+<div class="container bleh">
+<div class="row text-center">
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="guld.jpeg" alt="Guldpapper">
+          <p><strong>Medlem Guld</strong></p>
+          <p>Som medlem Guld får ni 30% rabatt vid köp av varor.</p><br>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="silver.jpeg" alt="Silverpapper">
+          <p><strong>Medlem Silver</strong></p>
+          <p>Som medlem Silver får ni 20% rabatt vid köp av varor.</p><br>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="brons.jpg" alt="Bronspapper" id="keh">
+          <p><strong>Medlem Brons</strong></p>
+          <p>Som medlem Brons får ni 10% rabatt vid köp av varor.</p><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="sortiment"></div><img src="KaffeCroi.jpeg" alt="Croissant brevid en kopp kaffe" id="sortim">
+  <div class="hLine1"></div>
+<div class="vLine1"></div>
+<div class="AB">
+<i> Ett brett sortiment av godsaker!</i>
+<p>Det finns ett brett sortiment av varor som ni kan välja mellan. Från bakelse till bröd, ni kommer finna vad ni letar efter. Dessutom är de nybakade 
+varje dag för den optimala njutfaktorn</p>
+</div>
+<div class="vLine2"></div>
+<div class="hLine2"></div>
+  
+  <div class="container bleh">
+<div class="row text-center">
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="pastry1.jpeg" alt="Kräm bakelse med choklad">
+          <p><strong>Ljuvliga bakelser!</strong></p>
+          <p>Ljuvliga bakelser som estetisk sätt ser underbara ut. Och smakar underbart också.</p><br>
+		  <a href="bakelse.html" id="linkcol">Till bakelse sortimentet</a>
+        </div>
+      </div>
+	  <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="bulle11.jpeg" alt="Vanlig bulle">
+          <p><strong>Saftiga bullar!</strong></p>
+          <p>Inga torra eller brända bullhar här, nej. Bara saftiga och goda bullar serveras.</p><br>
+		  <a href="bullar.html" id="linkcol">Till bulle sortimentet</a>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="thumbnail">
+          <img src="bread.jpeg" alt="Mörkt, rund bröd">
+          <p><strong>Fantastiska bröd!</strong></p>
+          <p>Bröd som får oss att äta och äta och fortsätta att äta är här. Smakar fantastiskt.</p><br>
+		  <a href="bröd.html" id="linkcol">Till bröd sortimentet</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<footer>
+<img src="Bbröd.png" alt="Logo: BB" id="fav">
+
+<div class="logoInfo"><h3>Bättre bröd AB</h3><p class="map">Bergslagsgatan 2 <br> 411 04 Göteborg</p><p class="brev">kontakt@kokboksklubben.se</p>
+<p class="telefon">0520-471 98</p> 
+</div>
+	
+<div class="öppet"><h3>Öppetider för butik</h3>
+    <p>Mån - Tors: 8 - 19</p><p>Fre - Lör: 8 - 20</p><p>Sön: 9 - 15</p>
+	</div>
+	
+	<div id="media"><h3>Följ oss på</h3>
+    <a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+	<a href="https://www.twitter.com"><i class="fab fa-twitter-square"></i></a>
+	<a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+	</div>
+	
+</footer>
+</body>
+</html>
