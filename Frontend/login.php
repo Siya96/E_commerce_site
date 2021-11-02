efa5b7881bf073326c062e02bf23d717df6383bb
Siyabend Revend <Siyarevend@hotmail.com>
2021-11-02 18:16:31 +0100

    Modified-2021-11-02

--- Frontend/login.php
+++ Frontend/login.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
+<!doctype html>
 
-  <meta charset="utf-8">
+<head>
+<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="structure.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
@@ -10,10 +9,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <link rel="icon" href="favicon.gif" type="image/gif" sizes="15x15">
-  
 </head>
-<body><div class="border-center"></div>
 
 <div class="bannerContainer">
 <nav class="navbar navbar-inverse">
@@ -36,7 +32,7 @@
 	<div class="collapse navbar-collapse" id="myNavbar">
 	
 	<ul class="nav navbar-nav">
-      <li><a href="index.php" id="active">Home</a></li>
+      <li><a href="index.php" id="menu">Home</a></li>
       <li class="dropdown"><a class="dropbtn" href="#" id="menu">Sortiment</a>
           <div class="dropdown-content link">
           <a href="bugatti.php">Bugatti</a>
@@ -44,7 +40,7 @@
 		  <a href="ford.php">Ford</a>
       </div></li>
 	  <li><a href="contact.php" id="menu">Contact us</a></li>
-      <li><a href="login.php" id="menu">Login</a></li>
+      <li><a href="login.php" id="active">Login</a></li>
     </ul>
 	
     <ul class="nav navbar-nav navbar-right">
@@ -59,48 +55,19 @@
   </div></div>
 </nav></div>
 
-
-
-<div class="jumbotron">
-
-</div><img src="blue_car_01.jpg" alt="A blue sportscar" id="first_pic">
-
-
-
-  
-  <div class="container bleh">
-<div class="row text-center">
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="bugatti.jpg" alt="Bugatti">
-          <p><strong>Bugatti</strong></p>
-          <p>Most valued Bugattis on the market.</p><br>
-		  <a href="bugatti.php" id="linkcol">To Bugatti sortiment</a>
-        </div>
-      </div>
-	  <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="ferrari.jpg" alt="Ferrari">
-          <p><strong>Ferrari</strong></p>
-          <p>The fastest Ferraris on the market.</p><br>
-		  <a href="ferrari.php" id="linkcol">To Ferrari sortiment</a>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="ford_gt.jpg" alt="Ford">
-          <p><strong>Ford</strong></p>
-          <p>Latest Ford models on the market</p><br>
-		  <a href="ford.php" id="linkcol">To Ford sortiment</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-<footer>
-    <img src="logga.gif" alt:"Logo: Sportscar" class="logo_gif">
-</footer>
+<div class="leftIn">
+<b>Sign in</b>
+<form action="inlogg.php" method="POST">
+<input type="text" name="usrnamne" placeholder="E-post">
+<input type="password" name="pwd" placeholder="Lösenord">
+<a href="glömt.php">Forgot your password?</a><br>
+<a href="reg.php"><button id="vidare">Create account</button></a>
+<button type="submit" id="vidare">Sign in</button>
+</form>
+</div>
 
 
 </body>
+
 </html>
+
