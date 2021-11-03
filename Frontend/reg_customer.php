bbb9251be6d3985784e645485c920c37c89724ad
Siyabend Revend <Siyarevend@hotmail.com>
2021-11-03 14:33:35 +0100

    Latest update

--- Frontend/reg_customer.php
+++ Frontend/reg_customer.php
@@ -1,9 +1,15 @@
 <!doctype html>
 
+<?php
+
+#require ('connect.inc.php');
+
+?>
+
 <head>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="login.css">
+  <link rel="stylesheet" type="text/css" href="reg_customer.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -56,18 +62,21 @@
   </div></div>
 </nav></div>
 
-<div class="leftIn">
-<b>Sign in</b>
-<form action="inlogg.php" method="POST">
-<input type="text" name="usrnamne" placeholder="E-post">
-<input type="password" name="pwd" placeholder="Lösenord">
-<a href="glömt.php">Forgot your password?</a><br>
-<button type="submit" id="vidare">Sign in</button>
+<div class="center">
+<b>Registrering</b>
+
+<form action="reg2.php" method="POST">
+<input type="text" name="fornamn" placeholder="Förnamn" required>
+<input type="text" name="efternamn" placeholder="Efternamn" required><br>
+<input type="email" name="epost" placeholder="E-post" required>
+<input type="email" name="bek_epost" placeholder="Bekräfta e-post" required><br>
+<input type="password" name="pwd" placeholder="Lösenord" required>
+<input type="password" name="bek_pwd" placeholder="Bekräfta lösenord" required><br>
+<input type="tel" name="tel" placeholder="Telefonnummer" required><br>
+<button type="submit" id="vidare" name="signup-submit">Registrera</button>
+
 </form>
-<a href="reg_customer.php"><button id="vidare">Create account</button></a>
 </div>
-
-
 
 <footer>
     <img src="logga.gif" alt:"Logo: Sportscar" class="logo_gif">
@@ -77,4 +86,3 @@
 </body>
 
 </html>
-
