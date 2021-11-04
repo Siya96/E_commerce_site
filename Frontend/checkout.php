7771630a2853bd5144d7cf7dc9fcb20ad206c7f8
Siyabend Revend <Siyarevend@hotmail.com>
2021-11-04 11:58:05 +0100

    Admin added

--- Frontend/checkout.php
+++ Frontend/checkout.php
@@ -2,14 +2,17 @@
 
 session_start();
 
-$items = array();
+    if(isset($_SESSION['shopping_cart']) && empty($_SESSION['shopping_cart'])) {
 
-$_SESSION['shopping_cart'] = $items;
+        ?>
+        <div class="shoppingCart">Your shopping cart is empty!</div>
+        <?php
+    }
+    else {
 
+
+    }
 ?>
-
-
-
 
 
 <!DOCTYPE html>
@@ -18,7 +21,7 @@ $_SESSION['shopping_cart'] = $items;
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="stylesheet" type="text/css" href="checkout.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -62,7 +65,6 @@ $_SESSION['shopping_cart'] = $items;
       </div></li>
 	  <li><a href="contact.php" id="menu">Contact us</a></li>
       <li><a href="login.php" id="menu">Login</a></li>
-      <li><a href="admin.php" id="menu">Admin</a></li>
     </ul>
 	
 
@@ -86,41 +88,7 @@ $_SESSION['shopping_cart'] = $items;
 
 
 
-<div class="jumbotron">
 
-</div><img src="blue_car_01.jpg" alt="A blue sportscar" id="first_pic">
-
-
-
-  
-  <div class="container bleh">
-<div class="row text-center">
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="bugatti.jpg" alt="Bugatti">
-          <p><strong>Bugatti</strong></p>
-          <p>Most valued Bugattis on the market.</p><br>
-		  <a href="bugatti.php" id="linkcol">To Bugatti sortiment</a>
-        </div>
-      </div>
-	  <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="ferrari.jpg" alt="Ferrari">
-          <p><strong>Ferrari</strong></p>
-          <p>The fastest Ferraris on the market.</p><br>
-		  <a href="ferrari.php" id="linkcol">To Ferrari sortiment</a>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="thumbnail">
-          <img src="ford_gt.jpg" alt="Ford">
-          <p><strong>Ford</strong></p>
-          <p>Latest Ford models on the market</p><br>
-		  <a href="ford.php" id="linkcol">To Ford sortiment</a>
-        </div>
-      </div>
-    </div>
-  </div>
 
 <footer>
     <img src="logga.gif" alt:"Logo: Sportscar" class="logo_gif">
