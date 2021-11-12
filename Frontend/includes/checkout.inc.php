4698a0f22f2d96fd02c74f70dcecca790469040a
astjam-9 <astjam-9@student.ltu.se>
2021-11-12 19:15:50 +0100

    Works

--- Frontend/includes/checkout.inc.php
+++ Frontend/includes/checkout.inc.php
@@ -16,29 +16,28 @@ if(isset($_SESSION["userID"])){
 	
 	if ($itemsInBasket->num_rows == 0){
 		echo "<h2>Empty basket.</h2>";
-		exit();
-	}
+	}else{
     
-    while($row = $itemsInBasket->fetch_assoc()){
+		while($row = $itemsInBasket->fetch_assoc()){
 		
-        $cartype = $row['car_type'];
-        $sql2 = "SELECT items.price FROM items WHERE items.car_type = '$cartype';";
-        $price = mysqli_query($connection, $sql2);
+			$cartype = $row['car_type'];
+			$sql2 = "SELECT items.price FROM items WHERE items.car_type = '$cartype';";
+			$price = mysqli_query($connection, $sql2);
         
-        $car_price = $price->fetch_assoc();
-        $totalAmount = $totalAmount + $car_price["price"];
-        echo '<h2>Product: '.$row["car_type"].'</h2>';
-        echo '<h2>Price: '.$car_price["price"].'</h2>';
-		echo '<form method="post">'; //NO action = submits to same page
-        echo '<input type="hidden" value="'.$row["basket_id"].'" name="basketID">';
-		echo '<button type="submit" name="removeFromBasket">Remove from basket</button>';
-		echo '</form><hr>';
-        //echo "Cartype:".$row['car_type']."Price:".$car_price['price'];
-    }
-    echo '<h2>TOTAL PRICE:'.$totalAmount. '</h2><form method="post"><button type="submit" name="purchase_button"> Purchase items </button> </form> </div>';
-}
-else
-{
+			$car_price = $price->fetch_assoc();
+			$totalAmount = $totalAmount + $car_price["price"];
+			echo '<h2>Product: '.$row["car_type"].'</h2>';
+			echo '<h2>Price: '.$car_price["price"].'</h2>';
+			echo '<form method="post">'; //NO action = submits to same page
+			echo '<input type="hidden" value="'.$row["basket_id"].'" name="basketID">';
+			echo '<button type="submit" name="removeFromBasket">Remove from basket</button>';
+			echo '</form><hr>';
+			//echo "Cartype:".$row['car_type']."Price:".$car_price['price'];
+		}
+		
+		echo '<h2>TOTAL PRICE:'.$totalAmount. '</h2><form method="post"><button type="submit" name="purchase_button"> Purchase items </button> </form> </div>';
+	}
+}else{
     echo "Not logged in";
 }
 
--- Frontend/includes/sortiment.inc.php
+++ Frontend/includes/sortiment.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'header.php';
+//include_once 'header.php';
 require_once 'db_handler.php';
 
 
